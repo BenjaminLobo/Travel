68267cec5b61336238a627da85f2a972e3bf42d0
Benjamin lobo <bvlobo2002@gmail.com>
2021-11-16 18:38:29 +0530

    Merge branch 'master' of https://github.com/BenjaminLobo/Travel
     Conflicts:
    	src/main/resources/com/example/travel/hello-view.fxml

---
+++ src/main/java/com/example/travel/transportationController.java
@@ -0,0 +1,23 @@
+package com.example.travel;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+public class transportationController {}
+
+/*public class transportationController {
+    ObservableList<String> carList = FXCollections.observableArrayList("Ola","Uber","Lyft");
+
+    @FXML
+    private ChoiceBox car;
+
+    @FXML
+    private void initialize(){
+        car.setValue("Ola");
+        car.setItems(carList);
+    }
+
+}*/
+
